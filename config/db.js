ec5af6c95651006ddf4aa155ed7a0b227c81381e
MisterKuper <102316501+MisterKuper@users.noreply.github.com>
2024-09-06 17:07:56 +0200

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kolishenkoroman:35537310@cluster0.0caax.mongodb.net/food-delivery-app').then(() => console.log('DB Connected'));
-}
